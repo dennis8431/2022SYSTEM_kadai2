6144924395570ed58831b7201e0b048c9957ccda
Ryukoutei <78011315+dennis8431@users.noreply.github.com>
2023-01-28 10:37:41 +0900

    Update login.php

--- public/login.php
+++ public/login.php
@@ -40,6 +40,10 @@ if (!empty($_POST['email']) && !empty($_POST['password'])) {
 }
 ?>
 
+<!-- スマートフォンでも見やすいデザインに -->
+<link href="./css/responsive.css" rel="stylesheet">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+
 <h1>ログイン</h1>
 
 初めての人は<a href="/signup.php">会員登録</a>しましょう。
