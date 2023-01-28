ef0ac379e5df34983b88e0926213a5e830a0eeea
Ryukoutei <78011315+dennis8431@users.noreply.github.com>
2023-01-28 10:36:49 +0900

    Update signup_finish.php

--- public/signup_finish.php
+++ public/signup_finish.php
@@ -1,3 +1,7 @@
+<!-- スマートフォンでも見やすいデザインに -->
+<link href="./css/responsive.css" rel="stylesheet">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+
 <h1>会員登録完了</h1>
 
 会員登録が完了しました。<br>
